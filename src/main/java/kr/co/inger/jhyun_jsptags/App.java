59dc5b1d414d89c519ea051cf31723bb6b964b8b
jonghyouk yun <ageldama@gmail.com>
2013-08-06 19:44:59 +0900

    inits

---
+++ src/main/java/kr/co/inger/jhyun_jsptags/App.java
@@ -0,0 +1,13 @@
+package kr.co.inger.jhyun_jsptags;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
